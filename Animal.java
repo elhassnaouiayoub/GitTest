e66dea9eccb22c36662b30a1f9eb6d240ce74f04
Ayoub El Hassnaoui <aelhassnaoui@smb.sqli.com>
2022-03-14 16:02:18 +0100

    last commit

--- Animal.java
+++ Animal.java
@@ -6,25 +6,17 @@ public class Animal {
     public static String getDefaultGreeting(){
         return "Hello";
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> bird
     public static String getCatGreeting(){
         return "meow!";
     }
     public static void main(String[] args) {
         String firstArgument = args[0];
         String output = "";
-<<<<<<< HEAD
-       /* if(firstArgument.equalsIgnoreCase("bird"))
-            output = getBirdGreeting();*/
-        if(firstArgument.equalsIgnoreCase("cat"))
-=======
+        
        if(firstArgument.equalsIgnoreCase("bird"))
             output = getBirdGreeting();
         else if(firstArgument.equalsIgnoreCase("cat"))
->>>>>>> bird
             output = getCatGreeting();
         else output = getDefaultGreeting();
 
