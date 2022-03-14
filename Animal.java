e041b01e1d967956e9bb2cdd6bfe5b773d59579c
Ayoub El Hassnaoui <aelhassnaoui@SMB.SQLI.COM>
2022-03-14 15:05:24 +0100

    The bird class is added

--- Animal.java
+++ Animal.java
@@ -1,23 +1,23 @@
 public class Animal {
-    /*public static String getBirdGreeting(){
+    public static String getBirdGreeting(){
         return "tweet";
     }
-*/
+
     public static String getDefaultGreeting(){
         return "Hello";
     }
-/*
+
     public static String getCatGreeting(){
         return "meow!";
-    }*/
+    }
     public static void main(String[] args) {
         String firstArgument = args[0];
         String output = "";
-       /* if(firstArgument.equalsIgnoreCase("bird"))
+       if(firstArgument.equalsIgnoreCase("bird"))
             output = getBirdGreeting();
         else if(firstArgument.equalsIgnoreCase("cat"))
-            output = getCatGreeting();*/
-        output = getDefaultGreeting();
+            output = getCatGreeting();
+        else output = getDefaultGreeting();
 
         System.out.println(output);
 
